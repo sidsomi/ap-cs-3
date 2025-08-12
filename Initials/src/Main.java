966480a27ff8de04dcedf823cadf84fb085975be
sidsomi <sidbond80@gmail.com>
2025-08-12 11:07:44 -0700

    Main.java

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -1,9 +1,9 @@
 import kareltherobot.*;
 
-public class Main implements Directions {
+public class Main implements Directions{
   public static void main(String[] args) {
-    World.setVisible(true);// allows us to see the run-
-    World.setDelay(2);
+    World.setVisible(true);// allows us to see the run
+ World.setDelay(2);
 
     // The line below creates a Robot that we will refer to as r.
     // Find out what the numbers and direction do!
@@ -53,5 +53,73 @@ public class Main implements Directions {
     r.turnLeft();
     r.turnLeft();
     r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.turnLeft();
+    r.turnLeft();
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.move();// move one step in the direction it is facing
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
+    r.move();// move one step in the direction it is facing
   }
 }
