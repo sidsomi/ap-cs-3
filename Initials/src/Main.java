01fcdbb581782b7421e983ee781364982104d088
sidsomi <sidbond80@gmail.com>
2025-08-13 12:04:54 -0700

    Update Main.java

--- Initials/src/Main.java
+++ Initials/src/Main.java
@@ -9,11 +9,35 @@ public class Main implements Directions{
     Robot r = new Robot(2,8,East,9);
 		
     // examples of commands you can invoke on a Robot
-    r.move();// move one step in the direction it is facing
-    r.turnLeft();
-    r.putBeeper();
+	  for (var i = 0; i < 3; i++) {
+        
+r.putBeeper();
     r.move();
-	r.putBeeper();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.turnLeft(); 
+    r.move();
+    r.putBeeper();
+    r.turnLeft();
+    r.turnLeft();
+    r.turnLeft();
+    r.move();
+    r.turnLeft();
+    r.move();
+    r.putBeeper();
 
   }
+
 }
