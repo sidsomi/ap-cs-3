0367d7b4e68c3c3ec5ea570307060bc61940628a
sidsomi <sidbond80@gmail.com>
2025-09-02 11:33:09 -0700

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,145 @@
+import java.util.Scanner;  //may be necessary for input
+
+import javax.swing.JOptionPane;  //may be necessary for input
+
+import kareltherobot.*;
+
+public class Driver implements Directions {
+// declared here so it is visible in all the methods!! 
+// It will be assigned a value in the getInfo method
+    private static Robot r; 
+
+
+    // You will add very many variables!!
+
+    
+    public static void main(String[] args) {
+        // LEAVE THIS ALONE!!!!!!
+        Driver d = new Driver();
+
+  /**
+     * This section of code gets info from the user in the following order: 1. Ask the user
+     * which world file they wish to process. Right now, that world file name is
+     * hardcoded in. 2. Ask the user for the starting location and direction of the
+     * Robot. A new Robot should be constructed and assigned to the global
+     * (instance) variable named r that is declared on line 10.
+     * 
+     * An inelegant way to interact with the user is via the console, using
+     * System.out.println and a Scanner that scans System.in (input from the
+     * console). A more elegant way to get user input might include using a
+     * JOptionPane.
+     */
+
+    String wrldName = "basicRoom.wld";
+
+        World.readWorld(wrldName);
+    World.setVisible(true);
+    World.setDelay(5);
+        Robot r = new Robot (7, 7, East, 0);
+while (r.frontIsClear()==true) {
+    r.move();
+    while (r.nextToABeeper()==true) {
+        r.pickBeeper();
+        
+    }
+}
+
+
+if (r. facingWest()==true) {
+	r.turnLeft();
+	r.turnLeft();
+	r.turnLeft();
+	r.move();
+	r.turnLeft();
+	r.turnLeft();
+	r.turnLeft();
+}
+if (r.facingEast()==true) {
+	r.turnLeft();
+	r.move();
+	r.turnLeft();
+}
+while (r.frontIsClear()==true) {
+    r.move();
+    while (r.nextToABeeper()==true) {
+        r.pickBeeper();
+        
+    }
+}
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+
+while (r.frontIsClear()==true) {
+    r.move();
+    while (r.nextToABeeper()==true) {
+        r.pickBeeper();
+        
+    }
+}
+r.turnLeft();
+r.move();
+r.turnLeft();
+while (r.frontIsClear()==true) {
+    r.move();
+    while (r.nextToABeeper()==true) {
+        r.pickBeeper();
+        
+    }
+}
+if (r.facingEast()) {
+    r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+r.move();
+r.turnLeft();
+r.turnLeft();
+r.turnLeft();
+}
+if (r.facingWest()) {
+    r.turnLeft();
+r.move();
+r.turnLeft();
+}
+
+while (r.frontIsClear()==true) {
+    r.move();
+    while (r.nextToABeeper()==true) {
+        r.pickBeeper();
+        }
+
+}
+    /** This section will have all the logic that takes the Robot to every location 
+     * and cleans up all piles of beepers.  Think about ways you can break this
+     * large, complex task into smaller, easier to solve problems.
+     */
+
+        // the line below causes a null pointer exception
+        // what is that and why are we getting it?
+        
+
+
+  
+
+
+
+
+
+    /** This method displays the results of cleaning the room.  All of the info
+     * in the pdf that describes the problem need to be displayed.  You can present
+     * this info in the console (boring) or you can present using JOptionPane (cool!)
+     */
+
+  
+
+
+  
+
+
+}
+
+    }
